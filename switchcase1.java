b02a70b8deba0341a768e76118d3d97798717fb9
Chandriki <120454846+Chandriki@users.noreply.github.com>
2023-05-05 11:24:31 -0700

    Add files via upload

---
+++ switchcase1.java
@@ -0,0 +1,30 @@
+package chandriki2;
+
+public class switchcase1 {
+public static void main(String args[])
+{
+	for(int i=0;i<6;i++)
+	{
+		switch(i)
+		{
+		case 0:
+			System.out.println("i is 0");
+			break;
+		case 1:
+			System.out.println("i is one");
+			break;
+		case 2:
+			System.out.println("i is two");
+			break;
+		case 3:
+			System.out.println("i is three");
+			break; 
+			default:
+				System.out.println("i is greater than 3");
+		}
+		
+		
+	}
+}
+}
+
